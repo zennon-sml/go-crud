b5862f21c5e40ba1d0bb503cbe8c2dcbab3d8e60
zennon-sml <zennonsampaioml@gmail.com>
2024-11-12 21:46:59 -0300

    connecting to db localhost

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/zennon-sml/go-crud
 
 go 1.23.2
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+  "log"
+  "database/sql"
   "fmt"
   "net/http"
+
+  _ "github.com/lib/pq"
 )
 
 func handlerGet(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +18,30 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+  const (
+    host = "localhost"
+    port = 5432
+    user = "zennon"
+    password = "asdf"
+    dbname = "gocrud"
+  )
+  dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+  db, err := sql.Open("postgres", dsn)
+  if err != nil {
+    log.Fatal("failed to connect to db")
+  } else {
+    fmt.Println("success to connect")
+  }
+  defer db.Close()
+
+  err = db.Ping()
+  if err != nil {
+    log.Fatal("failed to ping the db")
+  } else {
+    fmt.Println("success to ping")
+  }
+
   http.HandleFunc("GET /", handlerGet)
   http.HandleFunc("POST /", handlerPost)
   http.ListenAndServe(":8080", nil)
